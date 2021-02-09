7ba6129dfd3d298c4d649d3da335fa234935e562
Neophytos Michael <nmichael@gmail.com>
2021-02-09 22:48:06 +0200

    Simplify some function definitions in Utils and other cleanups.

--- src/Utils.hs
+++ src/Utils.hs
@@ -5,27 +5,29 @@ module Utils where
 
 import qualified Data.Map.Lazy as M
 import qualified Data.List as L
-import Data.Bits (Bits(shiftL, (.|.)))
+import qualified Data.Bits as Bits
 
 {-# INLINABLE maxBy #-}
 maxBy :: (a -> a -> Ordering) -> a -> a -> a
-maxBy cmp x y
- = if res == LT then y else x
-   where
-     res = cmp x y
+maxBy cmp x y = if cmp x y == LT then y else x
 
+{-# INLINABLE minBy #-}
+minBy :: (a -> a -> Ordering) -> a -> a -> a
+minBy cmp x y = if cmp x y == GT then y else x
+
+{-# INLINABLE calcMagicConstant #-}
 calcMagicConstant :: [Integer] -> Integer -> Integer
 calcMagicConstant xs bitsInWord = c
   where
-    one = 1 :: Integer
     bitsInW = bitsInWord - 1
-    c = L.foldl' (\acc x -> acc .|. shiftToPos x) (0 :: Integer) xs
-    shiftToPos n = one `shiftL` fromIntegral (bitsInW - n)
+    c = L.foldl' (\acc x -> (Bits..|.) acc (shiftToPos x)) (0 :: Integer) xs
+    shiftToPos n = Bits.shiftL (1 :: Integer) (fromIntegral (bitsInW - n))
 
 {-# INLINABLE calcRevMap #-}
 calcRevMap :: Ord b => M.Map a b -> M.Map b [a]
 calcRevMap = M.foldrWithKey' (\n label m -> M.insertWith (++) label [n] m) M.empty
 
+{-# INLINABLE isDense #-}
 isDense :: [Integer] -> Bool
 isDense [] = True
 isDense ns = len == span
@@ -37,12 +39,12 @@ isDense ns = len == span
     go :: [Integer] -> Int -> (Int, Integer)
     go [n] !len = (len, n)
     go (_ : ns) !len = go ns (len + 1)
-    go [] _ = error "The improssible happened!"
+    go [] _ = error "The impossible happened!"
 
+{-# INLINABLE isDenseEnough #-}
 isDenseEnough :: Integer -> [Integer] -> Bool
 isDenseEnough _gap [] = True
-isDenseEnough gap (cur : ns)
-  = go cur ns
+isDenseEnough gap (cur : ns) = go cur ns
   where
     go prev (cur : ns) = cur - prev <= gap && go cur ns
     go _prev [] = True
