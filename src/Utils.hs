f2a7a2562ca5af72cf7137768c120eac86c88044
Neophytos Michael <nmichael@gmail.com>
2021-02-09 21:47:23 +0200

    Mark some simple functions as INLINABLE.

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,6 +7,7 @@ import qualified Data.Map.Lazy as M
 import qualified Data.List as L
 import Data.Bits (Bits(shiftL, (.|.)))
 
+{-# INLINABLE maxBy #-}
 maxBy :: (a -> a -> Ordering) -> a -> a -> a
 maxBy cmp x y
  = if res == LT then y else x
@@ -21,6 +22,7 @@ calcMagicConstant xs bitsInWord = c
     c = L.foldl' (\acc x -> acc .|. shiftToPos x) (0 :: Integer) xs
     shiftToPos n = one `shiftL` fromIntegral (bitsInW - n)
 
+{-# INLINABLE calcRevMap #-}
 calcRevMap :: Ord b => M.Map a b -> M.Map b [a]
 calcRevMap = M.foldrWithKey' (\n label m -> M.insertWith (++) label [n] m) M.empty
 
@@ -45,5 +47,6 @@ isDenseEnough gap (cur : ns)
     go prev (cur : ns) = cur - prev <= gap && go cur ns
     go _prev [] = True
 
+{-# INLINABLE ind #-}
 ind :: Bool -> Int 
 ind b = if b then 1 else 0
