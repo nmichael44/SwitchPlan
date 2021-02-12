11c0959aeb3189b7daabc65612ff99e9135ed848
Neophytos Michael <nmichael@gmail.com>
2021-02-12 18:44:51 +0200

    New unit tests file.

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,46 +12,7 @@ import qualified Switch as SW
 import qualified Eval as EV
 import qualified Utils as U
 
-import Debug.Trace
-
-type Label = SW.Label 
-
-data PieceOfCase
-  = C (Integer, Label)  -- usual case i.e. n -> L1
-    | D Label       -- the default case
-
-newtype DataTypeRange = DTR (Integer, Integer)
-newtype TestingRange = TR (Integer, Integer)
-
-data TestCase = TestCase [PieceOfCase] DataTypeRange TestingRange SW.SwitchTargets SW.Platform
-
-mkTestCase :: [PieceOfCase] -> DataTypeRange -> TestingRange -> SW.Platform -> TestCase
-mkTestCase cases dtr@(DTR (lb, ub)) tr@(TR (testLb, testUb)) platform
-  = let
-      fullSet = S.fromAscList [lb..ub]
-      intToLabel =
-        case U.buildMap (cases >>=  (\case { (C p) -> [p]; (D _) -> [] })) of
-          Left err -> error err
-          Right m -> m
-      numCases = M.size intToLabel
-
-      defaults = concatMap (\case { (C _) -> []; (D l)-> [l] }) cases
-      numDefaults = length defaults
-    in
-      if | lb > ub -> error "Bad data type range."
-         | testLb > testUb -> error "Bad data type range."
-         | numCases == 0 -> error "No cases specified."
-         | numDefaults > 1 -> error "Too many defaults"
-         | numCases == 1 && numDefaults == 0 -> error "Missing default case."
-         | not $ S.isSubsetOf (M.keysSet intToLabel) (S.fromAscList [lb..ub]) -> error "Cases outside of datatype range."
-         | S.difference fullSet (M.keysSet intToLabel) /= S.empty && numDefaults /= 1 -> error "Missing cases but no default specified."
-         | fullSet == M.keysSet intToLabel && numDefaults == 1 -> error "Default specified when no gaps."
-         | otherwise ->
-           let
-             defLabelOpt = if numDefaults == 0 then Nothing else Just $ head defaults
-             st = SW.mkSwitchTargets True (lb, ub) defLabelOpt intToLabel
-           in
-             TestCase cases dtr tr st platform
+import Debug.Trace ( trace )
 
 lb :: Int
 lb = 0
@@ -135,7 +96,7 @@ prop_switch_plan_test
             tr (show plan) $
             collect (isBitTest plan) $
             collect noBitAtTop $
-            doTest lb ub platform st plan
+            doTest2 lb ub platform st plan
 
 label0 = SW.L 0
 label1 = SW.L 1
@@ -143,16 +104,15 @@ label1 = SW.L 1
 buildPlan :: Int -> Int -> SW.Platform -> S.Set Integer -> S.Set Integer -> (SW.SwitchTargets, SW.SwitchPlan)
 buildPlan lb ub platform s0 s1
   = trace (show st) $
-    trace (show plan) $
+    trace (show plan)
     (st, plan)
   where
-    platform = SW.Platform SW.bytesInWordForPlatform
     ls = [(s, label0) | s <- S.toList s0] ++ [(s, label1) | s <- S.toList s1]
     st = SW.mkSwitchTargets True (fromIntegral lb, fromIntegral ub) Nothing (M.fromList ls)
     plan = SW.createPlan st platform
 
-doTest :: Int -> Int -> SW.Platform -> SW.SwitchTargets -> SW.SwitchPlan -> Bool
-doTest start end platform st plan
+doTest2 :: Int -> Int -> SW.Platform -> SW.SwitchTargets -> SW.SwitchPlan -> Bool
+doTest2 start end platform st plan
   = go start
   where
     (SW.SwitchTargets _ _ _ intToLabel labelToInts _) = st
